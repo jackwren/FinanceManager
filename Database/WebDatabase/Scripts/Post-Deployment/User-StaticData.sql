cb648033a80706c30b7f8349ffd425e42a73a2e6
jackwren <jack_wr@hotmail.co.uk>
2017-10-13 10:32:05 +0100

    Creted DB project within the solution
    Added test and db solution

---
+++ Database/WebDatabase/Scripts/Post-Deployment/User-StaticData.sql
@@ -0,0 +1,16 @@
+﻿DECLARE @username VARCHAR(50)
+DECLARE @password VARCHAR(50)
+
+SET @username = 'test'
+SET @password = 'test'
+
+IF NOT EXISTS(SELECT 1 FROM [dbo].[Users]
+				WHERE username = 'test')
+BEGIN
+	INSERT INTO [dbo].[Users]([username], [password])
+	SELECT @username, @password
+END
+ELSE 
+BEGIN 
+	PRINT 'No data to insert'
+END
---
+++ Database/WebDatabase/obj/Debug/postdeploy.sql
@@ -0,0 +1,29 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+DECLARE @username VARCHAR(50)
+DECLARE @password VARCHAR(50)
+
+SET @username = 'test'
+SET @password = 'test'
+
+IF NOT EXISTS(SELECT 1 FROM [dbo].[Users]
+				WHERE username = 'test')
+BEGIN
+	INSERT INTO [dbo].[Users]([username], [password])
+	SELECT @username, @password
+END
+ELSE 
+BEGIN 
+	PRINT 'No data to insert'
+END
+GO

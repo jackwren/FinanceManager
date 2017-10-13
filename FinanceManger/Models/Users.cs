ecf4c8da50c1964c50c9764f2f2a4e95a1b9124d
jackwren <jack_wr@hotmail.co.uk>
2017-10-13 16:42:18 +0100

    Added entity framework to connect to Db

---
+++ FinanceManger/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinanceManger.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+    }
+}
